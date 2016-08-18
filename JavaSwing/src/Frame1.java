e6c5be69c52d83c892b8ce13e79e6298de7df62b
lamphongthanh1992 <lamphongthanh1992@gmail.com>
2016-08-18 17:57:33 +0700

    cdd

--- JavaSwing/src/Frame1.java
+++ JavaSwing/src/Frame1.java
@@ -1,6 +1,8 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
 
 
 public class Frame1 {
@@ -37,6 +39,9 @@ public class Frame1 {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewButtion = new JButton("New Buttion");
+		frame.getContentPane().add(btnNewButtion, BorderLayout.NORTH);
 	}
 
 }
